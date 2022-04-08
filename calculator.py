6dd85e455d57c99563b4d53b8fdb67f26a433412
sterrettm <sterretm@oregonstate.edu>
2022-04-08 11:33:44 -0700

    Added all the folders that were supposed to be here

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def sum(x, y):
+    return x + y
+
+def product(x, y):
+    return x * y
+
+from math import sqrt
---
+++ lab2.py
@@ -0,0 +1,8 @@
+import calculator
+
+def prob1(L):
+    return min(L), max(L), sum(L) / len(L)
+
+def prob2():
+    print()
+
